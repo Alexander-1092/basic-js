f78f1e6d79f0e1d3e15a68124eb808b55ad4e575
Alexander-1092 <Alex-Sadr1092@yandex.ru>
2023-12-24 19:48:42 +0300

    feat: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,89 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+	let newStr = ''
+
+	if(options.repeatTimes !== undefined){
+
+		for (let index = 0; index < options.repeatTimes; index++) {
+			newStr += String(str);
+	
+			if(options.additionRepeatTimes !== undefined){
+				
+				for (let j = 0; j < options.additionRepeatTimes; j++) {
+					newStr += options.addition
+					if (j !== options.additionRepeatTimes - 1){
+						if(options.additionSeparator !== undefined){
+							newStr += options.additionSeparator
+						}	else if (options.additionSeparator === undefined) {
+							newStr += '|'
+						}
+					}
+				}
+			} else if (options.addition !== undefined) {
+				newStr += options.addition
+			}
+	
+			if (options.repeatTimes !== undefined){
+				if(index !== options.repeatTimes - 1 && options.separator !== undefined){
+					newStr += options.separator;
+				}else if(options.repeatTimes > 1 && index !== options.repeatTimes - 1){
+					newStr += '+'
+				}
+			}
+		}
+
+	} else {
+
+		for (let index = 0; index < 1; index++) {
+			newStr += str;
+	
+			if(options.additionRepeatTimes !== undefined){
+	
+				for (let j = 0; j < options.additionRepeatTimes; j++) {
+					newStr += options.addition
+					if (j !== options.additionRepeatTimes - 1){
+						if(options.additionSeparator !== undefined){
+							newStr += options.additionSeparator
+						}	else if (options.additionSeparator === undefined) {
+							newStr += '|'
+						}
+					}
+				}
+			} 
+			if(options.additionRepeatTimes === undefined){
+	
+				for (let j = 0; j < 1; j++) {
+					newStr += options.addition
+					if (j !== options.additionRepeatTimes - 1){
+						if(options.additionSeparator !== undefined && options.additionRepeatTimes !== undefined){
+							newStr += options.additionSeparator
+						}	else if (options.additionSeparator === undefined) {
+							newStr += '|'
+						}
+					}
+				}
+			} 
+
+	
+			if (options.repeatTimes !== undefined){
+	
+				if(index !== options.repeatTimes - 1 && options.separator !== undefined){
+					newStr += options.separator;
+				}else if(options.repeatTimes > 1 && index !== options.repeatTimes - 1){
+					newStr += '+'
+				}
+			}
+		}
+
+	}
+
+
+
+	return newStr
 }
+
 
 module.exports = {
   repeater
